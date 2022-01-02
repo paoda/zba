eb37d73cb27cf6aec195675a40f886d97a370c08
Rekai Musuka <rekai@musuka.dev>
2022-01-02 14:57:59 -0600

    chore: panic on read from BIOS
    GBA Bios requires a lot of implemented features, so we're ignoring it
    for now

--- src/bus/bios.zig
+++ src/bus/bios.zig
@@ -17,14 +17,17 @@ pub const Bios = struct {
     }
 
     pub inline fn get32(self: *const @This(), idx: usize) u32 {
+        std.debug.panic("[BIOS] TODO: BIOS is not implemented", .{});
         return (@as(u32, self.buf[idx + 3]) << 24) | (@as(u32, self.buf[idx + 2]) << 16) | (@as(u32, self.buf[idx + 1]) << 8) | (@as(u32, self.buf[idx]));
     }
 
     pub inline fn get16(self: *const @This(), idx: usize) u16 {
+        std.debug.panic("[BIOS] TODO: BIOS is not implemented", .{});
         return (@as(u16, self.buf[idx + 1]) << 8) | @as(u16, self.buf[idx]);
     }
 
     pub inline fn get8(self: *const @This(), idx: usize) u8 {
+        std.debug.panic("[BIOS] TODO: BIOS is not implemented", .{});
         return self.buf[idx];
     }
 };
