1d4ba2e2b362dbda4c4d2a48a723d34951425253
Rekai Musuka <rekai@musuka.dev>
2022-01-03 19:46:24 -0600

    fix(emu): prevent infinite loop when advancing scheduler

--- src/emu.zig
+++ src/emu.zig
@@ -1,19 +1,17 @@
-const _ = @import("std");
-
 const Scheduler = @import("scheduler.zig").Scheduler;
 const Arm7tdmi = @import("cpu.zig").Arm7tdmi;
 const Bus = @import("bus.zig").Bus;
 
-const cycles_per_frame: u64 = 10_000; // TODO: How many cycles actually?
+const cycles_per_frame: u64 = 100; // TODO: How many cycles actually?
 
 pub fn runFrame(sch: *Scheduler, cpu: *Arm7tdmi, bus: *Bus) void {
-    const frame_end = sch.tick + cycles_per_frame;
+    var cycles: u64 = 0;
+    while (cycles < cycles_per_frame) : (cycles += 1) {
+        sch.tick += 1;
+        _ = cpu.step();
 
-    while (sch.tick < frame_end) {
-        while (sch.tick < sch.nextTimestamp()) {
-            sch.tick += cpu.step();
+        while (sch.tick >= sch.nextTimestamp()) {
+            sch.handleEvent(cpu, bus);
         }
-
-        sch.handleEvent(cpu, bus);
     }
 }
