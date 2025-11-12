497a62c16dfab3b62b3f56d28b4d596743e82c25
Rekai Musuka <rekai@musuka.dev>
2025-11-12 17:18:45 -0600

    fix: make sure to pass in optimize to exe in build.zig

--- build.zig
+++ build.zig
@@ -39,6 +39,7 @@ pub fn build(b: *std.Build) void {
         // Later on we'll use this module as the root module of a test executable
         // which requires us to specify a target.
         .target = target,
+        .optimize = optimize,
     });
 
     // Here we define an executable. An executable needs to have a root module
